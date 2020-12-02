3bdc31f9f70e3723fcbc68d4325b14afc013be76
CRISTIAN AMIR CAMELO ARIZA <cristian.camelo@segurosbolivar.com>
2020-12-02 13:09:07 -0500

    [API][ADD] Se crea el controlador para la configuración de convenios y entidades

---
+++ API/PagosDesdeArchivosAPI/app/src/main/java/com/bolivar/pagos/Controllers/configureEntities.java
@@ -0,0 +1,41 @@
+package pagos;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class configureEntities {
+
+    @GetMapping(value = "/configureEntities")
+    public ResponseEntity<String> getEntities(){
+        return new ResponseEntity<String>("getEntities", HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/configureEntity")
+    public ResponseEntity<String> createEntity(){
+        return new ResponseEntity<String>("createEntity", HttpStatus.CREATED);
+    }
+
+    @GetMapping(value = "/configureEntity")
+    public ResponseEntity<String> getEntityById(@RequestParam("id") String id){
+        return new ResponseEntity<String>("getEntityById " + id, HttpStatus.OK);
+    }
+
+    @PutMapping(value = "configureEntity")
+    public ResponseEntity<String> updateEntity(@RequestParam("id") String id){
+        return new ResponseEntity<String>("updateEntity " , HttpStatus.ACCEPTED);
+    }
+
+    //@RequestMapping("/uploadFile")
+    public String inicio() {
+        return "Estamos en /api/alguploadFileo";
+    }
+}
