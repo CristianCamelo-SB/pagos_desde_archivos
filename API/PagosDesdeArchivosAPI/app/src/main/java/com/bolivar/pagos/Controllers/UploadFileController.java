8b35e9681087c7dbb2b07fc3c3c1ac5384eb89ad
CRISTIAN AMIR CAMELO ARIZA <cristian.camelo@segurosbolivar.com>
2020-12-09 10:35:44 -0500

    [API][ADD] Se agrega la funcionalidad de recibir archivos

--- API/PagosDesdeArchivosAPI/app/src/main/java/com/bolivar/pagos/Controllers/UploadFileController.java
+++ API/PagosDesdeArchivosAPI/app/src/main/java/com/bolivar/pagos/Controllers/UploadFileController.java
@@ -1,18 +1,40 @@
 package pagos;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class UploadFileController {
 
-    @PostMapping(value = "/uploadFile")
-    public ResponseEntity<String> uploadFile(){
-        return new ResponseEntity<String>("uploadFile", HttpStatus.CREATED);
+    @CrossOrigin(origins = "http://localhost:4200")
+    @RequestMapping(value = "/uploadFile", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> uploadFile(@RequestParam("files") MultipartFile files){
+        try {
+            //for(int i=0; i<files.size(); i++){
+                File convertFile = new File(files.getOriginalFilename());
+                convertFile.createNewFile();
+                FileOutputStream fout = new FileOutputStream(convertFile);
+                fout.write(files.getBytes());
+                fout.close();
+            //}
+            return new ResponseEntity<String>("uploadFile", HttpStatus.CREATED);
+        } catch (IOException e) {
+            //TODO: handle exception
+            return new ResponseEntity<String>("uploadFile", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        
     }
 }
