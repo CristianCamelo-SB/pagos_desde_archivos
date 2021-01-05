d6895809e644cb7204b6ce040bc9064532b64cd1
CRISTIAN AMIR CAMELO ARIZA <cristian.camelo@segurosbolivar.com>
2021-01-05 15:02:45 -0500

    [FRONT][ADD] Se configuran las rutas y vistas

--- Frontend/PagosDesdeArchivosFrontAdmin/src/app/app-routing.module.ts
+++ Frontend/PagosDesdeArchivosFrontAdmin/src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { ConfigureEntitiesListComponent } from './components/configure-entities-list/configure-entities-list.component';
+import { ConfigureEntitiesFormComponent } from './components/configure-entities-form/configure-entities-form.component';
+import { CommissionReportListComponent } from './components/commission-report-list/commission-report-list.component';
+import { CommissionReportFormComponent } from './components/commission-report-form/commission-report-form.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/configureEntities',
+    pathMatch: 'full'
+  },
+  {
+    path: 'configureEntities',
+    component: ConfigureEntitiesListComponent
+  },
+  {
+    path: 'configureEntities/:id',
+    component: ConfigureEntitiesFormComponent
+  },
+  {
+    path: 'configureEntities/edit/:id',
+    component: ConfigureEntitiesFormComponent
+  },
+  {
+    path: 'generateReport',
+    component: CommissionReportListComponent
+  },
+  {
+    path: 'generateReport/:id',
+    component: CommissionReportFormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
